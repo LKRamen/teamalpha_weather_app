f50541d2d2a831618a5663e823acb5f695ef5f29
Layth Rahman <laythrahman11@gmail.com>
2024-05-21 11:54:08 -0400

    Separated into 3 major functions. One to prompt the user, one to format and print all the format information, and one to reset if the user pleases.

--- main.py
+++ main.py
@@ -2,31 +2,46 @@
 Create a weather app
 """
 import requests
-import json
+import datetime
+import sys
 
 def main():
-    print("Weather Forecast")
-    city_name = input("Please enter a city:\n")
-    state_name = input("Please enter this city's state:\n")
-    geocode_json = requests.get("https://geocode.maps.co/search?city=" + city_name + "&state=" + state_name + "&api_key=664b7f0e168b8295284849yinb13482").json()
-    longitude = geocode_json[0]['lon']
-    latitude = geocode_json[0]['lat']
-    weather_json = requests.get("http://api.weatherapi.com/v1/forecast.json?key=af49b45544a4460385c173117242005&q=" + latitude + "," + longitude + "&days=5").json()
-    forecast_dates = [str(weather_dict['date']) for weather_dict in weather_json['forecast']['forecastday']]
-    forecast_avgtemps = [str(weather_dict['day']['avgtemp_f'] for weather_dict in weather_json['forecast']['forecastday']
-    #with open("weather_output.json", "w") as output_file:
-    #    json.dump(weather_json, output_file, indent=6)
-    #    print(output_file)
-def get_forecast():
-    print("Weather Forecast")
-    city_name = input("Please enter a city:\n")
-    state_name = input("Please enter this city's state:\n")
-    geocode_json = requests.get(
-        "https://geocode.maps.co/search?city=" + city_name + "&state=" + state_name + "&api_key=664b7f0e168b8295284849yinb13482").json()
-    longitude = geocode_json[0]['lon']
-    latitude = geocode_json[0]['lat']
-    weather_json = requests.get("http://api.weatherapi.com/v1/forecast.json?key=af49b45544a4460385c173117242005&q=" + latitude + "," + longitude + "&days=5").json()
-    return
+    weather_json = prompt_user()
+    format_forecast(weather_json)
+    reset()
 
-if __name__=="__main__":
+def prompt_user() -> dict:
+    city_name = input("Please enter a city:\n")
+    state_name = input("Please enter this city's state:\n")
+    try:
+        geocode_json = requests.get("https://geocode.maps.co/search?city=" + city_name + "&state=" + state_name + "&api_key=664b7f0e168b8295284849yinb13482").json()
+        longitude = geocode_json[0]['lon']
+        latitude = geocode_json[0]['lat']
+        weather_json = requests.get("http://api.weatherapi.com/v1/forecast.json?key=af49b45544a4460385c173117242005&q=" + latitude + "," + longitude + "&days=5").json()
+        return weather_json
+    except:
+        print("Sorry the city or state named was invalid. Please try again.\n")
+        main()
+
+def format_forecast(weather_json: dict) -> str:
+    forecast_year = [weather_dict['date'][0:4] for weather_dict in weather_json['forecast']['forecastday']]
+    forecast_month = [weather_dict['date'][5:7] for weather_dict in weather_json['forecast']['forecastday']]
+    forecast_day = [weather_dict['date'][8:10] for weather_dict in weather_json['forecast']['forecastday']]
+    date_datetime = datetime.date(int(forecast_year[0]), int(forecast_month[0]), int(forecast_day[0]))
+    day = date_datetime.strftime('%a')
+    print(day)
+    forecast_avgtemps = [str(weather_dict['day']['avgtemp_f']) for weather_dict in weather_json['forecast']['forecastday']]
+
+def reset() -> None:
+    reset_input = input("Would you like to search a different forecast? Enter 'y' for Yes, 'n' for No\n")
+    if reset_input == 'y':
+        main()
+    elif reset_input == 'n':
+        sys.exit("Thank you for using this forecast")
+    else:
+        print("Sorry that was an invalid answer\n")
+        reset()
+
+if __name__=='__main__':
+    print("This is a Weather Forecast")
     main()
